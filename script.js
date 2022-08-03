44d1349cc9110351a3c40cf5cfabcdf52a85c7e8
newerk <juniusowens@yahoo.com>
2022-08-03 12:15:30 -0400

    Add Func That Builds Card CSS Styling

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
 let main = document.querySelector('.main');
+let bkTitle = document.querySelector("#bk_title");
+let bkAuthor = document.querySelector("#bk_author");
+let bkPages = document.querySelector("#bk_pages");
 
-let theHobbit = {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pages: 295,
-    read: "not read yet",
-    info: function info() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    }
-}
+let myLibrary = [];
 
-
-let myLibrary = [theHobbit];
-
-function Book() {
+function Book(title, author, pages) {
     //the constructor..
-
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function addBookToLibrary() {
     //do stuff here
-    // myLibrary.push(Book);
+    let userInputs = Object.create(Book.prototype);
+    userInputs.title = bkTitle.value;
+    userInputs.author = bkAuthor.value;
+    userInputs.pages = bkPages.value;
+    myLibrary.push(userInputs);
+
+    let card = document.createElement('div');
+    card.className = 'book';
+    card.textContent = bkTitle.value;
+    // main.append(card);
+
+    buildCard();
+
+
+
+}
+
+function buildCard() {
+    let card = document.createElement('div');
+    card.className = 'book';
+
+    let title = document.createElement('div');
+    title.textContent = bkTitle.value;
+
+    let author = document.createElement('div');
+    author.textContent = bkAuthor.value;
+
+    let pages = document.createElement('div');
+    pages.textContent = bkPages.value;
+
+
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+
+
+    main.append(card);
 
 }
 
 
-//build a "card" that diplays the book on the website
+//displays every object in the library. certain funtions will update the display. if a book is removed, the display needs to reflect that. of a book is 
 function displayBooks() {
-    let card = document.createElement('div');
-    card.className = 'book';
-    card.textContent = "THE HOBBIT";
-    main.append(card);
-
 }
